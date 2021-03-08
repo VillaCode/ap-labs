cf0ebda8b7c50888f767e93bbb0b515f332c08cd
Kevin Villa <kevinvillab89@gmail.com>
2021-03-08 00:47:09 +0000

    solved reverser
    Signed-off-by: Kevin Villa <kevinvillab89@gmail.com>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *reverse(char str[], int n){
+    char *new = malloc(50);
+    int j = 0;
+    for (int i = n; i >= 0; i--)
+    {
+        new[j] = str[i];
+        j += 1;
+    }
+    return new;
+}
+
 
 int main(){
-    // Place your magic here
+
+    char str[50], ch;
+    int i;
+    printf("Pasa una string: \n");
+    i = 0;
+    ch = getchar();
+
+    while (ch != EOF)
+    {
+        str[i] = ch;
+        i++;
+        ch = getchar();
+        if (ch == '\n')
+        {
+            char *reversed;
+            reversed = reverse(str, i);
+            printf("String al reves %s\n", reversed);
+            ch = getchar();
+            i = 0;
+            memset(str, 1,50);
+        }
+    }
     return 0;
 }
+
+
